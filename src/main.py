7af2c2d6c268660fde6dbdd309731a0250dfa8d8
unicornfairy <unicornfairy864@gmail.com>
2025-04-28 17:02:34 +0800

    Add connection between F and B

--- src/main.py
+++ src/main.py
@@ -11,5 +11,5 @@ from config import port, global_dir
 from server import app
 
 if __name__ == '__main__':
-    window = webview.create_window('GTCS Console By Ficshl', app)
+    window = webview.create_window('GTCS CSS Client', app)
     webview.start()
--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from config import port, global_dir
 
@@ -9,6 +9,10 @@ app = Flask(__name__, template_folder = os.path.join(global_dir, "templates"))
 @app.route('/')
 def index():
     return render_template('client.html', port = port)
+
+@app.route('/refresh', methods=['POST'])
+def refresh():
+    return request.form.get("cmdList")
 
 if __name__ == '__main__':
     app.run(debug=True, port = port)
